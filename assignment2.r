518ed281f79785410fe8ef5dec30f83e20929362
bokajgd <54889907+bokajgd@users.noreply.github.com>
2022-03-02 13:35:14 +0100

    updates to loop

--- assignment2.r
+++ assignment2.r
@@ -1,10 +1,11 @@
  library(pacman)
-pacman::p_load(tidyverse, here, posterior, cmdstanr, brms)
+pacman::p_load(tidyverse, here, posterior, cmdstanr, brms, sigmoid)
 
 
 # Read data
-data  <- read_csv("Desktop/Cognitive_Science/Cognitive Science 8th Semester/Advanced Cognitive Modeling/Week 3 - Stan/game_data.csv")
+d  <- read_csv("Desktop/Cognitive_Science/Cognitive Science 8th Semester/Advanced Cognitive Modeling/Week 3 - Stan/game_data_rate_loops.csv")
 
+data <- d %>% subset(rate == 0.7)
 
 # Function for converting log odds to probability
 logit2prob <- function(logit){
@@ -39,8 +40,8 @@ for (p in seq(nrow(priors))){
   d <- list(
     n = nrow(data),
     h = data$agent_choices,
-    stay_bias = data$Staybias,
-    leave_bias = data$Leavebias,
+    heads_bias = data$Staybias,
+    tails_bias = data$Leavebias,
     alpha_prior_mean=priors$alpha_prior_mean[p],
     beta_prior_mean=priors$beta_prior_mean[p],
     beta2_prior_mean=priors$beta2_prior_mean[p],
@@ -64,6 +65,12 @@ for (p in seq(nrow(priors))){
   temp <- tibble(alpha = draws_df$alpha,
                  beta = draws_df$beta,
                  beta2 = draws_df$beta2,
+                 alpha_sd = samples$summary()[2,4],
+                 beta_sd = samples$summary()[3,4],
+                 beta2_sd = samples$summary()[4,4],
+                 alpha_prior = draws_df$alpha_prior,
+                 beta_prior = draws_df$beta_prior,
+                 beta2_prior = draws_df$beta2_prior,
                  alpha_prior_mean=priors$alpha_prior_mean[p],
                  beta_prior_mean=priors$beta_prior_mean[p],
                  beta2_prior_mean=priors$beta2_prior_mean[p],
@@ -76,3 +83,35 @@ for (p in seq(nrow(priors))){
 
 samples$summary()
 
+# Plotting sensitivity for alpha
+sensitivity_df %>% ggplot(aes(as.factor(alpha_prior_mean), alpha))+
+  geom_point(size=3, alpha=0.05)+
+  facet_wrap(~alpha_prior_sd)+
+  theme_classic()
+
+# Plotting sensitivity for alpha
+sensitivity_df %>% ggplot(aes(as.factor(beta_prior_mean), beta))+
+  geom_point(size=3, alpha=0.05)+
+  facet_wrap(~beta_prior_sd)+
+  theme_classic()
+
+# Plotting sensitivity for alpha
+sensitivity_df %>% ggplot(aes(as.factor(beta2_prior_mean), beta2))+
+  geom_point(size=3, alpha=0.05)+
+  facet_wrap(~beta2_prior_sd)+
+  theme_classic()
+
+
+sensitivity_df %>% subset(alpha_prior_mean==0 & alpha_prior_sd==1 &
+                          beta_prior_mean==0 & beta_prior_sd==.5 &
+                          beta2_prior_mean==0 & beta2_prior_sd==.5) %>% 
+  ggplot() +
+  geom_density(aes(beta, fill="blue", alpha=0.3) +
+  geom_density(aes(beta_prior), fill="red", alpha=0.3) +
+  xlab("Rate") +
+  ylab("Posterior Density") +
+  theme_classic()
+
+  
+theta_test <- samples$summary()[2,2] + samples$summary()[3,2] * (-1)
+sigmoid(1)
